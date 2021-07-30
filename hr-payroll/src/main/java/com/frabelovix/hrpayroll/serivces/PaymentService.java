c515c91a4f9a86044a9c63767a114f9d7d14c84f
frabelovix <frabelovix@gmail.com>
2021-07-29 21:43:45 -0300

    RestTemplate - Comunicação entre serviço

--- hr-payroll/src/main/java/com/frabelovix/hrpayroll/serivces/PaymentService.java
+++ hr-payroll/src/main/java/com/frabelovix/hrpayroll/serivces/PaymentService.java
@@ -1,14 +1,33 @@
 package com.frabelovix.hrpayroll.serivces;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.frabelovix.hrpayroll.entities.Payment;
+import com.frabelovix.hrpayroll.entities.Worker;
 
 @Service
 public class PaymentService {
+	
+	@Value("${hr-worker.host}")
+	private String workerHost;
+	
+	@Autowired
+	private RestTemplate restTemplate;
 
 	public Payment getPayment(long workerId, int days) {
-		return new Payment("Mock", 200.0, 10);
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", ""+workerId);
+
+		
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
+		
 	}
 
 }
